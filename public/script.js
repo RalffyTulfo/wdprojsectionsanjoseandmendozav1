3194a5bf9d5ddaeab5cf237ad48d8c9cda8aeaa7
Glitch (glitch-hello-node) <none>
2024-12-22 01:59:46 +0000

    👠🎎 Checkpoint ./public/script.js:70058449/1345

--- public/script.js
+++ public/script.js
@@ -1,21 +1,45 @@
 const apt = {
   image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/Apt.jpg?v=1734829795018",
-  color: "#f2688c"
+  color: "#f2688c",
+  textColor: "#291b17"
 }
 const dieWithASmile = {
   image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/DieSmile.jpg?v=1734829826264",
-  color: "#2f88b5"
+  color: "#2f88b5",
+  textColor: "#f1ede9"
 }
 const anEveningWithSilkSonic = {
   image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/SilkSonic.png?v=1734779491655",
-  color: "#cd9b64"
+  color: "#cd9b64",
+  textColor: "#6d4238"
 }
 const blow = {
   image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/Blow.jpg?v=1734829799060",
-  color: "#fafafa"
+  color: "#fafafa",
+  textColor: "#291b17"
 }
 const pleaseMe = {
   image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/PleaseMe.png?v=1734829836651",
+  color: "#2f2355",
+  textColor: "#f1ede9"
+}
+const wakeUpInTheSky = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/WakeUpSky.jpg?v=1734829845944",
+  color: "#0a0605",
+  textColor: "#f1ede9"
+}
+const magic24k = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/24kMagic.jpg?v=1734779468894",
+  color: "#ba282a",
+  textColor: "#f1ede9"
+}
+const unorthodoxJukebox = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/UnorthoJuke.jpg?v=1734779496578",
+  color: "#d9ceb9",
+  textColor: "#921f22"
+}
+const itWillRain = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/ItWillRain.jpg?v=1734829854447",
   color: 
 }
 function changeColor(color) {
