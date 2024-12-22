c304b314b9381ab1d36a0cb9993cf1613dc16ad5
Glitch (glitch-hello-node) <none>
2024-12-22 01:47:47 +0000

    🌜📷 Checkpoint ./public/script.js:70058449/934

--- public/script.js
+++ public/script.js
@@ -1,4 +1,23 @@
-
+const apt = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/Apt.jpg?v=1734829795018",
+  color: "#f2688c"
+}
+const dieWithASmile = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/DieSmile.jpg?v=1734829826264",
+  color: "#2f88b5"
+}
+const anEveningWithSilkSonic = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/SilkSonic.png?v=1734779491655",
+  color: "#cd9b64"
+}
+const blow = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/Blow.jpg?v=1734829799060",
+  color: "#fafafa"
+}
+const pleaseMe = {
+  image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/PleaseMe.png?v=1734829836651",
+  color: 
+}
 function changeColor(color) {
   document.body.style.backgroundColor = color;
 }
