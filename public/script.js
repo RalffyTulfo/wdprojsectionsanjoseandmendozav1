6a2843b0871df7c4c696f5230390175240def824
Glitch (glitch-hello-node) <none>
2024-12-23 12:56:49 +0000

    🌇🍫 Checkpoint ./public/script.js:70058449/2169:70058507/30

--- public/script.js
+++ public/script.js
@@ -153,6 +153,86 @@ const discography = [
   }
 ];
 
+const albumsOnlyDiscography = [
+  {
+    name: "Doo Wops & Hooligans",
+    image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/DooWops.jpg?v=1734779476447",
+    color: '#fbf0c4',
+    textColor: '#291b17',
+    date: 2010,
+    streams: 245,
+    songs: [
+      "Just the Way You Are",
+      "Grenade",
+      "The Lazy Song",
+      "Marry You",
+      "Count on Me",
+      "Talking to the Moon",
+      "Runaway Baby",
+      "Liquor Store Blues (feat. Damian Marley)",
+      "The Other Side (feat. CeeLo Green and B.o.B)",
+      "Our First Time"
+    ]
+  },
+  {
+   name: "Unorthodox Jukebox",
+    image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/UnorthoJuke.jpg?v=1734779496578",
+    color: '#d9ceb9',
+    textColor: '#921f22',
+    date: 2012,
+    streams: 242,
+    songs: [
+      "Locked Out of Heaven",
+      "Treasure",
+      "When I Was Your Man",
+      "Gorilla",
+      "If I Knew",
+      "Moonshine",
+      "Natalie",
+      "Show Me",
+      "Money Make Her Smile",
+      "Young Girls"
+    ]
+  },
+  {
+    name: "24k Magic",
+    image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/24kMagic.jpg?v=1734779468894",
+    color: '#ba282a',
+    textColor: '#f1ede9',
+    date: 2016,
+    streams: 239,
+    songs: [
+      "24K Magic",
+      "That's What I Like",
+      "Finesse",
+      "Versace on the Floor",
+      "Chunky",
+      "Perm",
+      "Straight Up & Down",
+      "Too Good to Say Goodbye",
+      "Calling All My Lovelies"
+    ]
+  },
+  {
+    name: "An Evening With Silk Sonic",
+    image: "https://cdn.glitch.global/28cd22da-8213-4d82-b974-08837fa87097/SilkSonic.png?v=1734779491655",
+    color: '#cd9b64',
+    textColor: '#6d4238',
+    date: 2021,
+    streams: 143,
+    songs: [
+      "Leave the Door Open",
+      "Fly as Me",
+      "After Last Night (with Thundercat and Bootsy Collins)",
+      "Smokin Out the Window",
+      "Put On a Smile",
+      "Skate",
+      "777",
+      "Blast Off"
+    ]
+  }
+]
+
 function changeColor(color) {
   document.body.style.backgroundColor = color;
 }
@@ -217,31 +297,33 @@ function displayAlbums(sortedAlbums){
   albumContainer.innerHTML = ""; // Clears display
 
   // Go through each album and append
-  for (var i = 0; i < sortedAlbums.length - 1; i++) {
+  for (var i = 0; i < sortedAlbums.length; i++) {
     var album = sortedAlbums[i];
     var div = document.createElement("div"); // Create a new <div> element
+    var img = document.createElement("img");
     div.style.margin = "10px 0"; // Margin for spacing
 
     // Set the text content
-    div.textContent = album.name + " (" + album.date + ") - " + album.streams + "M streams";
+    img.src = album.image;
+    div.textContent = album.name + " (" + album.date + ") - " + album.streams + "0M streams";
 
     // Append to the albumContainer
-    albumContainer.appendChild(div);
+    albumContainer.appendChild(img);
   }
 }
 
 // Show all albums initially
-displayAlbums(discography.image);
+displayAlbums(albumsOnlyDiscography);
 
 // Conditional function based on dropdown
 function handleSort(option) {
-  if (option === 'date-asc') {
+  if (option === 'dasc') {
     sortAlbums('asc');
-  } else if (option === 'date-desc') {
+  } else if (option === 'ddesc') {
     sortAlbums('desc');
-  } else if (option === 'stream-asc') {
+  } else if (option === 'pasc') {
     sortStreams('asc');
-  } else if (option === 'stream-desc') {
+  } else if (option === 'pdesc') {
     sortStreams('desc');
   }
 }
